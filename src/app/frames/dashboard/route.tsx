acae23205c80aed3cd18e7daae1c946329e9f894
nkemjika <nkemjikaohanyere@gmail.com>
2024-05-22 13:13:39 +0100

    refactor: fix mistake with style

--- src/app/frames/dashboard/route.tsx
+++ src/app/frames/dashboard/route.tsx
@@ -127,11 +127,11 @@ const handler = frames(async (ctx) => {
         return {
             image: (
                 <div tw="flex flex-col relative w-full h-full items-center justify-center m-auto">
-                    <div tw="flex flex-col relative w-3/4 w-2/3 items-center justify-center">
+                    <div tw="flex flex-col relative w-3/4 h-2/3 items-center justify-center">
                         {displayedObject.map(({ subject, value }, index) => (
                             <div
                                 key={index}
-                                tw="flex flex-row relative w-full border border-gray-800 justify-between px-2 py-3"
+                                tw="flex flex-row relative w-full border border-gray-800 justify-between px-2 "
                             >
                                 <p>{subject}:</p>
                                 <p tw="ml-2">{value}</p>
