8008ba80dcd29ece83397f8f3cb22e2427c838b1
nkemjika <nkemjikaohanyere@gmail.com>
2024-05-22 14:06:52 +0100

    add a bit of description

--- src/app/frames/route.tsx
+++ src/app/frames/route.tsx
@@ -3,12 +3,16 @@ import { Button } from "frames.js/next";
 import { frames } from "./frames";
 
 const handleRequest = frames(async () => {
-    const actionUrl = `https://warpcast.com/~/add-cast-action?${new URLSearchParams({ url: new URL("/frames/actionMeta", "https://superlike-action.vercel.app").toString() })}`;
+    const actionUrl = `https://warpcast.com/~/add-cast-action?${new URLSearchParams({ url: new URL("/frames/actionMeta", "http:localhost:3000").toString() })}`;
 
     return {
         image: (
             <div tw="flex relative w-full h-full items-center justify-center">
                 PowerLike Action ⚡️
+                <p tw="text-sm mt-2 w-full">
+                    Use action on any cast to power like it. <br /> Use frame to
+                    tip all power liked casts at once.{" "}
+                </p>
                 <div tw="bottom-0 right-0 absolute bg-gray-800 border-t-4 border-r-4 border-gray-800 rounded-tl-2xl p-4 text-white text-2xl">
                     By @nkemjika
                 </div>
