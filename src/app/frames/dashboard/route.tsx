172664aff2b79f7f8d411dd6a23c7cc145b513c3
nkemjika <nkemjikaohanyere@gmail.com>
2024-05-22 21:56:06 +0100

    fix

--- src/app/frames/dashboard/route.tsx
+++ src/app/frames/dashboard/route.tsx
@@ -142,11 +142,10 @@ const handler = frames(async (ctx) => {
                                     ),
                                 )}
                             </ul>
-
-                            <span tw="text-md mt-4">
-                                Leave textfield empty to tip 100%
-                            </span>
                         </div>
+                        <span tw="text-md mt-4">
+                            Leave textfield empty to tip 100%
+                        </span>
                     </div>
 
                     <div tw="bottom-0 right-0 absolute bg-gray-800 border-t-4 border-r-4 border-gray-800 rounded-tl-2xl p-4 text-white text-2xl">
