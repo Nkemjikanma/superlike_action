8541851348434e0bfa5cb9ef53e2f3fb3099197a
nkemjika <nkemjikaohanyere@gmail.com>
2024-05-22 13:04:51 +0100

    style: fix padding on dashboard

--- src/app/frames/dashboard/route.tsx
+++ src/app/frames/dashboard/route.tsx
@@ -127,11 +127,11 @@ const handler = frames(async (ctx) => {
         return {
             image: (
                 <div tw="flex flex-col relative w-full h-full items-center justify-center m-auto">
-                    <div tw="flex flex-col relative w-3/4 h-4/5 items-center justify-center">
+                    <div tw="flex flex-col relative w-3/4 items-center justify-center">
                         {displayedObject.map(({ subject, value }, index) => (
                             <div
                                 key={index}
-                                tw="flex flex-row relative w-full border border-gray-800 justify-between px-2"
+                                tw="flex flex-row relative w-full border border-gray-800 justify-between px-2 py-3"
                             >
                                 <p>{subject}:</p>
                                 <p tw="ml-2">{value}</p>
