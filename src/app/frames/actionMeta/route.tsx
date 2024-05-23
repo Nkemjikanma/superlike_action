c89983e37f5710de832b9ef7d42ae9d9d65dcddf
nkemjika <nkemjikaohanyere@gmail.com>
2024-05-23 23:00:16 +0100

    fix: another attempt at fixing

--- src/app/frames/actionMeta/route.tsx
+++ src/app/frames/actionMeta/route.tsx
@@ -24,71 +24,43 @@ export const POST = frames(async (ctx) => {
     // let user: UserType;
     const { message } = ctx;
 
-    if (!message) {
+    if (message) {
+        const { requesterFid, castId } = message;
+
+        if (!castId) {
+            return Response.json({
+                message:
+                    "Will not PowerLike a cast without a castId - i'll try again",
+            });
+        }
+        const { data, error } = await getQuery(castId.fid);
+
+        console.log("before db query", castId.hash);
+
+        const queryDataResults = await queryLikesData(
+            castId.hash,
+            requesterFid,
+            castId.fid,
+        );
+
+        if (queryDataResults === "Like found") {
+            return Response.json({
+                message: `Cast by ${error ? ctx.message?.castId?.fid : data.Socials.Social[0].profileName} has already been PowerLiked`,
+            });
+        }
+
+        if (queryDataResults === "Like error") {
+            return Response.json({
+                message: "Error, try again later",
+            });
+        }
+
         return Response.json({
-            message: "Error, try again",
-        });
-    }
-
-    const { requesterFid, castId } = message;
-
-    if (!castId) {
-        return Response.json({
-            message:
-                "Will not PowerLike a cast without a castId - i'll try again",
-        });
-    }
-    const { data, error } = await getQuery(castId.fid);
-
-    console.log("before db query", castId.hash);
-
-    const queryDataResults = await queryLikesData(
-        castId.hash,
-        requesterFid,
-        castId.fid,
-    );
-
-    if (queryDataResults === "Like found") {
-        return Response.json({
-            message: `Cast by ${error ? ctx.message?.castId?.fid : data.Socials.Social[0].profileName} has already been PowerLiked`,
-        });
-    }
-
-    if (queryDataResults === "Like error") {
-        return Response.json({
-            message: "Error, try again later",
+            message: `Cast by ${error ? ctx.message?.castId?.fid : data.Socials.Social[0].profileName} has been PowerLiked`,
         });
     }
 
     return Response.json({
-        message: `Cast by ${error ? ctx.message?.castId?.fid : data.Socials.Social[0].profileName} has been PowerLiked`,
+        message: "No message found",
     });
-
-    // if (like && like.castId === castId.hash.toString()) {
-    //     // console.log("them don like am oo");
-    //     // return Response.json({
-    //     //     message: `Cast by ${error ? ctx.message?.castId?.fid : data.Socials.Social[0].profileName} has already been PowerLiked`,
-    //     // });
-    // } else {
-    //     console.log("them never like am oo");
-    //     const createLike = await prismadb.likes.create({
-    //         data: {
-    //             fid: requesterFid,
-    //             castId: castId.hash,
-    //             authorFid: castId.fid,
-    //             alreadyTipped: false,
-    //         },
-    //     });
-
-    //     if (!createLike) {
-    //         return Response.json({
-    //             message: "Error, try again",
-    //         });
-    //     }
-
-    //     console.log("final response to know why");
-    //     return Response.json({
-    //         message: `Cast by ${error ? ctx.message?.castId?.fid : data.Socials.Social[0].profileName} has been PowerLiked`,
-    //     });
-    // }
 });
